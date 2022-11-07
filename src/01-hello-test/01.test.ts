53f346b65119ddffceba6bdb0efc1fac5702656f
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-07 20:29:32 +0200

    created multi function test

--- src/01-hello-test/01.test.ts
+++ src/01-hello-test/01.test.ts
@@ -1,9 +1,23 @@
-import {sum} from "./01";
+import {mult, sum} from "./01";
 
-test('sum should be correct', ()=>{
-    const a= 1;
-    const b= 2;
-    const res= sum(a, b)
-    expect<number>(res).toBe(3);
-})
+test('sum should be correct', () => {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+    const res1 = sum(a, b);
+    const res2= sum(a, c);
 
+    expect(res1).toBe(3);
+    expect(res2).toBe(4);
+});
+
+test('multiply should be correct', () => {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+    const res1 = mult(a, b);
+    const res2= mult(a, c);
+
+    expect(res1).toBe(2);
+    expect(res2).toBe(3);
+});
