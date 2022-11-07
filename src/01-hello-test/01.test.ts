e6760157b1668be87b68badf45a269c9e2adf951
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-11-07 20:59:46 +0200

    added function beforeEach()

--- src/01-hello-test/01.test.ts
+++ src/01-hello-test/01.test.ts
@@ -1,23 +1,46 @@
-import {mult, sum} from "./01";
+import {mult, splitWord, sum} from "./01";
+
+let a: number;
+let b: number;
+let c: number;
+
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+});
 
 test('sum should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
     const res1 = sum(a, b);
-    const res2= sum(a, c);
+    const res2 = sum(a, c);
 
     expect(res1).toBe(3);
     expect(res2).toBe(4);
 });
 
 test('multiply should be correct', () => {
-    const a = 1;
-    const b = 2;
-    const c = 3;
+
     const res1 = mult(a, b);
-    const res2= mult(a, c);
+    const res2 = mult(a, c);
 
     expect(res1).toBe(2);
     expect(res2).toBe(3);
 });
+
+test('Func should return correct array of string', () => {
+    const str1 = 'Hello world';
+    const str2 = 'Hi my friends!';
+    const res1 = splitWord(str1);
+    const res2 = splitWord(str2);
+
+    expect(res1.length).toBe(2);
+    expect(res1[0]).toBe('Hello');
+    expect(res1[1]).toBe('world');
+
+    expect(res2.length).toBe(3);
+    expect(res2[0]).toBe('Hi');
+    expect(res2[1]).toBe('my');
+    expect(res2[2]).toBe('friends');
+
+});
+
--- src/01-hello-test/01.ts
+++ src/01-hello-test/01.ts
@@ -2,3 +2,7 @@
 export const sum = (a:number, b:number):number=> a+b ;
 
 export const mult = (a:number, b:number):number=> a*b ;
+
+export const splitWord = (str:string):string[] =>{
+    return ['Hello', 'world'];
+}
